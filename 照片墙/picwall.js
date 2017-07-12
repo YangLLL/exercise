996d9c574f3fc448e9c36f1e60de61786c593138
YangLLL <1599670483@qq.com>
2017-07-12 16:43:32 +0800

    照片墙

---
+++ 照片墙/picwall.js
@@ -0,0 +1,27 @@
+function chgsize(range){
+	var img1 = document.getElementsByTagName("img");
+	img1[0].style.width = 400*range + 'px';
+	img1[0].style.height = 300*range + 'px';
+	img1[1].style.width = 400*range + 'px';
+	img1[1].style.height = 300*range + 'px';
+	img1[2].style.width = 400*range + 'px';
+	img1[2].style.height = 300*range + 'px';
+	img1[3].style.width = 400*range + 'px';
+	img1[3].style.height = 300*range + 'px';
+	img1[4].style.width = 400*range + 'px';
+	img1[4].style.height = 300*range + 'px';
+	img1[5].style.width = 400*range + 'px';
+	img1[5].style.height = 300*range + 'px';
+	img1[6].style.width = 400*range + 'px';
+	img1[6].style.height = 300*range + 'px';
+	img1[7].style.width = 400*range + 'px';
+	img1[7].style.height = 300*range + 'px';
+	img1[8].style.width = 400*range + 'px';
+	img1[8].style.height = 300*range + 'px';
+	img1[9].style.width = 400*range + 'px';
+	img1[9].style.height = 300*range + 'px';
+}
+function chgcolor(mycolor){
+	var div1 = document.getElementById("mywall");   
+	div1.style.backgroundColor = mycolor;
+}
